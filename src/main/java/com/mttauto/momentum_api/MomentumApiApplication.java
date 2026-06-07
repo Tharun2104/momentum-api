ff89751fce83053c0e6ae53a68aca89bf93d10dc
Tharun2104 <tharunmogili09@gmail.com>
2026-06-07 11:16:16 -0400

    Initial Spring Boot setup

---
+++ src/main/java/com/mttauto/momentum_api/MomentumApiApplication.java
@@ -0,0 +1,13 @@
+package com.mttauto.momentum_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MomentumApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MomentumApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mttauto/momentum_api/MomentumApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mttauto.momentum_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MomentumApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
